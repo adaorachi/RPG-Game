2d122ab171cbbb096d72efdf854646e582c74960
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-05-13 04:30:35 +0100

    create bootScene class and functions

---
+++ src/Scenes/BootScene.js
@@ -0,0 +1,35 @@
+import Phaser from 'phaser';
+
+export default class BootScene extends Phaser.Scene {
+  constructor() {
+    super('Boot');
+  }
+
+  preload() {
+    this.loadImages();
+    this.loadSpriteSheets();
+    this.loadAudio();
+  }
+
+  loadImages() {
+    this.load.image('button1', 'assets/images/ui/blue_button01.png');
+    this.load.image('button2', 'assets/images/ui/blue_button02.png');
+  }
+
+  loadSpriteSheets() {
+    this.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('characters', 'assets/images/characters.png', { frameWidth: 32, frameHeight: 32 });
+  }
+
+  loadAudio() {
+    this.load.audio('goldSound', ['assets/audio/Pickup.wav']);
+  }
+
+  // loadTileMap() {
+  //   this.load.tilemapTiledJSON('map', 'assets/level/large_level.json');
+  // }
+
+  create() {
+    this.scene.start('Title');
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
 import Phaser from 'phaser';
-import config from './Config/config';
-import GameScene from './Scenes/GameScene';
+// import config from './config/config';
+import BootScene from './scenes/BootScene';
+import TitleScene from './scenes/TitleScene';
+import GameScene from './scenes/GameScene';
+import UiScene from './scenes/UiScene';
+// import OptionsScene from './scenes/OptionsScene';
+// import CreditsScene from './scenes/CreditsScene';
+// import PreloaderScene from './scenes/PreloaderScene';
 
-class Game extends Phaser.Game {
-  constructor() {
-    super(config);
-    this.scene.add('Game', GameScene);
-    this.scene.start('Game');
-  }
-}
+// class Game extends Phaser.Game {
+//   constructor() {
+//     super(config);
+//     this.scene.add('Boot', BootScene);
+//     this.scene.add('Preloader', PreloaderScene);
+//     this.scene.add('Title', TitleScene);
+//     this.scene.add('Options', OptionsScene);
+//     this.scene.add('Credits', CreditsScene);
+//     this.scene.add('Game', GameScene);
+//     this.scene.start('Game');
+//   }
+// }
 
-window.game = new Game();
+// window.game = new Game();
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [
+    BootScene,
+    TitleScene,
+    GameScene,
+    UiScene,
+  ],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+      gravity: {
+        y: 0,
+      },
+    },
+  },
+};
+
+const game = new Phaser.Game(config);
