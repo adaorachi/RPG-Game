8d96ed557c4fd62eea4d0bdef828703309a5f124
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-05-19 05:17:11 +0100

    fix linter errors

--- src/classes/player/Player.js
+++ src/classes/player/Player.js
@@ -1,6 +1,5 @@
-import Phaser from 'phaser';
-
 // eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies
+import Phaser from 'phaser';
 
 export default class Player extends Phaser.Physics.Arcade.Image {
   constructor(scene, x, y, key, frame) {
